c30f39ee7fa7610e368bfffe276e45f8fef6d67f
A. Jesse Jiryu Davis <jesse@emptysquare.net>
2023-10-30 17:28:17 -0400

    add noop writer, assert no stale reads

--- write-wait/simulate.py
+++ write-wait/simulate.py
@@ -1,9 +1,9 @@
-import collections
 import heapq
+import inspect
+import logging
 import random
 import typing
 from dataclasses import dataclass, field
-from typing import Coroutine
 
 Timestamp = int
 
@@ -14,37 +14,77 @@ def get_current_ts() -> Timestamp:
     return _current_ts
 
 
+def initiate_logging() -> None:
+    class CustomFormatter(logging.Formatter):
+        def format(self, record):
+            global _current_ts
+            original_msg = super().format(record)
+            return f"{_current_ts} {original_msg}"
+
+    formatter = CustomFormatter(fmt="%(levelname)s: %(message)s")
+    logging.basicConfig(level=logging.INFO)
+    for h in logging.getLogger().handlers:
+        h.setFormatter(formatter)
+
+
 class Future:
+    def __init__(self):
+        self.callbacks: list[typing.Callable] = []
+        self.resolved = False
+
+    def add_done_callback(self, callback: typing.Callable):
+        assert not inspect.iscoroutinefunction(callback), "Use create_task()"
+        if self.resolved:
+            callback()
+        else:
+            self.callbacks.append(callback)
+
+    def resolve(self):
+        self.resolved = True
+        callbacks = self.callbacks
+        self.callbacks = []  # Prevent recursive resolves.
+        for c in callbacks:
+            c()
+
     def __iter__(self):
         yield self
 
     __await__ = __iter__
 
 
-def _random_delay() -> int:
-    return random.randint(0, 10)
+class _Task(Future):
+    def __init__(self, coro: typing.Coroutine):
+        super().__init__()
+        self.coro = coro
+        self.step()
+
+    def step(self) -> None:
+        try:
+            f = self.coro.send(None)
+        except StopIteration:
+            self.resolve()  # Resume coroutines stopped at "await task".
+            return
+
+        assert isinstance(f, Future)
+        f.add_done_callback(self.step)
 
 
-@dataclass
-class Sleep(Future):
-    duration: int
-
-    @classmethod
-    def random_duration(cls):
-        return cls(_random_delay())
+def sleep_random(max_delay: int) -> Future:
+    f = Future()
+    _global_loop.call_later(max_delay=max_delay, callback=f.resolve)
+    return f
 
 
-class Loop:
+class EventLoop:
     @dataclass(order=True)
     class _Alarm:
         deadline: Timestamp
-        coro: Coroutine = field(compare=False)
+        callback: typing.Callable = field(compare=False)
 
     def __init__(self):
-        self._running = True
-        self._runnable: collections.deque[Coroutine] = collections.deque()
-        # List of (deadline, coroutine).
-        self._alarms: list[Loop._Alarm] = []
+        self._running = False
+        # Priority queue of scheduled alarms.
+        self._alarms: list[EventLoop._Alarm] = []
 
     def run(self):
         global _current_ts
@@ -53,29 +93,55 @@ class Loop:
         while self._running:
             if _current_ts > 1e6:
                 raise Exception(f"Timeout, current timestamp is {_current_ts}")
-            if self._runnable:
-                gen = self._runnable.popleft()
-            elif self._alarms:
-                alarm: Loop._Alarm = heapq.heappop(self._alarms)
+
+            if len(self._alarms) > 0:
+                alarm: EventLoop._Alarm = heapq.heappop(self._alarms)
                 # Advance time to next alarm.
+                assert alarm.deadline >= _current_ts
                 _current_ts = alarm.deadline
-                gen = alarm.coro
+                alarm.callback()
             else:
                 return  # All done.
-            try:
-                result = gen.send(None)
-            except StopIteration:
-                pass
-            else:
-                if isinstance(result, Sleep):
-                    heapq.heappush(
-                        self._alarms,
-                        Loop._Alarm(_current_ts + result.duration, gen))
 
     def stop(self):
         self._running = False
 
-    def call_soon(self, fn: typing.Callable, *args, **kwargs):
-        heapq.heappush(
-            self._alarms,
-            Loop._Alarm(_current_ts + _random_delay(), fn(*args, **kwargs)))
+    def call_soon(self, callback: typing.Callable, *args, **kwargs) -> Future:
+        """Schedule a callback as soon as possible.
+
+        Returns a Future that will be resolved after the callback runs.
+        """
+        return self.call_later(0, callback, *args, **kwargs)
+
+    def call_later(
+        self, max_delay: int, callback: typing.Callable, *args, **kwargs
+    ) -> Future:
+        """Schedule a callback after a random delay.
+
+        Returns a Future that will be resolved after the callback runs.
+        """
+        assert not inspect.iscoroutinefunction(callback), "Use create_task()"
+        f = Future()
+
+        def alarm_callback():
+            callback(*args, **kwargs)
+            f.resolve()
+
+        delay = random.randint(0, max_delay)
+        alarm = EventLoop._Alarm(deadline=_current_ts + delay, callback=alarm_callback)
+        heapq.heappush(self._alarms, alarm)
+        return f
+
+    def create_task(self, coro: typing.Coroutine) -> Future:
+        """Start running a coroutine.
+
+        Returns a Future that will be resolved after the coroutine finishes.
+        """
+        return _Task(coro)
+
+
+_global_loop = EventLoop()
+
+
+def get_event_loop() -> EventLoop:
+    return _global_loop
--- write-wait/write-wait.py
+++ write-wait/write-wait.py
@@ -1,11 +1,20 @@
 import bisect
+import logging
 import statistics
 from dataclasses import dataclass, field
 from enum import Enum, auto
 
-from simulate import Loop, Sleep, get_current_ts, Timestamp
+from simulate import (
+    get_event_loop,
+    get_current_ts,
+    initiate_logging,
+    sleep_random,
+    Timestamp,
+)
 
+logger = logging.getLogger("write-wait")
 D = 5  # Write-wait duration in milliseconds.
+MAX_DELAY = 10 * D
 
 
 class Role(Enum):
@@ -13,51 +22,54 @@ class Role(Enum):
     SECONDARY = auto()
 
 
-class Result(Enum):
-    OK = auto()
-    ERROR = auto()
+@dataclass(order=True)
+class OpTime:
+    """A hybrid logical clock (HLC)."""
+
+    ts: Timestamp
+    i: int
+
+    @classmethod
+    def default(cls) -> "OpTime":
+        return OpTime(-1, -1)
 
 
-@dataclass
+@dataclass(order=True)
 class Write:
-    key: str
-    ts: Timestamp
-
-
-@dataclass
-class LogEntry:
-    write: Write
-    ts: Timestamp
-
-    def __lt__(self, other: "LogEntry"):
-        # For bisect's sake.
-        return self.ts < other.ts
+    key: str = field(compare=False)
+    optime: OpTime
 
 
 @dataclass
 class Node:
     role: Role
-    data: dict[str, Timestamp] = field(default_factory=dict)
-    """A key-value store. Values are actually the last-written timestamp."""
-    log: list[LogEntry] = field(default_factory=list)
-    committed_ts: Timestamp = -1
-    last_applied_entry: LogEntry | None = None
+    data: dict[str, OpTime] = field(default_factory=dict)
+    """Data is a kv store. Values are actually the last-written optime."""
+    log: list[Write] = field(default_factory=list)
+    committed_optime: OpTime = field(default_factory=OpTime.default)
+    last_applied_entry: Write | None = None
     nodes: list["Node"] | None = None
-    node_replication_positions: dict[int, Timestamp] = field(
-        default_factory=dict)
+    node_replication_positions: dict[int, OpTime] = field(default_factory=dict)
     """Map Node ids to their last-replicated timestamps."""
-    _loop: Loop | None = None
 
-    def initiate(self, loop: Loop, nodes: list["Node"]):
-        self._loop = loop
+    def initiate(self, nodes: list["Node"]):
         self.nodes = nodes[:]
-        self.node_replication_positions = {id(n): -1 for n in nodes}
-        loop.call_soon(self.replicate, nodes)
+        self.node_replication_positions = {id(n): OpTime.default() for n in nodes}
+        get_event_loop().create_task(self.noop_writer())
+        get_event_loop().create_task(self.replicate())
 
-    async def replicate(self, nodes: list["Node"]):
-        peers = [n for n in nodes if n is not self]
+    async def noop_writer(self):
         while True:
-            await Sleep.random_duration()
+            await sleep_random(MAX_DELAY)
+            if self.role is not Role.PRIMARY:
+                continue
+
+            await self.write("noop")
+
+    async def replicate(self):
+        peers = [n for n in self.nodes if n is not self]
+        while True:
+            await sleep_random(MAX_DELAY)
             if self.role is Role.PRIMARY:
                 continue
 
@@ -67,93 +79,109 @@ class Node:
                 continue  # No primary.
 
             if len(self.log) >= len(primary.log):
+                # TODO: rollback if my log is longer than primary's.
                 continue
 
+            # Find the next entry to replicate and apply.
             if len(self.log) == 0:
                 i = 0
             else:
                 i = bisect.bisect_right(primary.log, self.log[-1])
 
             entry = primary.log[i]
-            self.data[entry.write.key] = entry.write.ts
+            self.data[entry.key] = entry.optime
             self.log.append(entry)
-            self.node_replication_positions[id(self)] = entry.ts
-            await Sleep.random_duration()
-            # Don't use call_soon, this can't handle out-of-order messages yet.
-            primary.update_secondary_position(self, entry.ts)
+            self.node_replication_positions[id(self)] = entry.optime
+            await sleep_random(MAX_DELAY)
+            # Don't use call_later, this can't handle out-of-order messages yet.
+            primary.update_secondary_position(self, entry.optime)
 
-    def update_secondary_position(self, secondary: "Node", ts: Timestamp):
+    def update_secondary_position(self, secondary: "Node", optime: OpTime):
         if self.role is not Role.PRIMARY:
             return
 
         # TODO: what about delayed messages / rollback?
-        self.node_replication_positions[id(secondary)] = ts
-        self.committed_ts = statistics.median(
-            self.node_replication_positions.values())
+        self.node_replication_positions[id(secondary)] = optime
+        self.committed_optime = statistics.median(
+            self.node_replication_positions.values()
+        )
+
         for n in self.nodes:
             if n is not self:
-                self._loop.call_soon(n.update_committed_ts,
-                                     self.committed_ts)
+                get_event_loop().call_later(
+                    MAX_DELAY, n.update_committed_optime, self.committed_optime
+                )
 
-    async def update_committed_ts(self, ts: Timestamp):
-        self.committed_ts = max(self.committed_ts, ts)
+    def update_committed_optime(self, optime: OpTime):
+        self.committed_optime = max(self.committed_optime, optime)
 
-    async def write(self, key: str):
-        write_start = get_current_ts()
-        w = Write(key=key, ts=write_start)
+    async def write(self, key: str) -> OpTime:
+        """Update a key and return the write's OpTime."""
+        optime = OpTime(get_current_ts(), 0)
+        if len(self.log) > 0 and self.log[-1].optime.ts == optime.ts:
+            optime.i = self.log[-1].optime.i + 1
+
+        w = Write(key=key, optime=optime)
         if self.role is not Role.PRIMARY:
-            return Result.ERROR
+            raise Exception("Not primary")
 
-        self.data[w.key] = w.ts
-        # TODO: Try a random sleep before appending the log entry.
-        self.log.append(LogEntry(write=w, ts=write_start))
-        self.node_replication_positions[id(self)] = write_start
-        while self.committed_ts < write_start:
+        self.data[w.key] = w.optime
+        # TODO: Try a realistic oplog, reserve a future slot and eventually fill it.
+        self.log.append(w)
+        self.node_replication_positions[id(self)] = optime
+        while self.committed_optime < optime:
             # TODO: abort on role change or rollback.
-            await Sleep.random_duration()
+            await sleep_random(MAX_DELAY)
 
         # TODO: abort on role change or rollback.
-        await Sleep(write_start + D - get_current_ts())
-        return Result.OK
+        await sleep_random(MAX_DELAY)
+        return optime
 
-    async def read(self, key: str):
+    async def read(self, key: str) -> OpTime:
+        """Return a key's last write OpTime."""
         query_start = get_current_ts()
         if self.role is Role.SECONDARY:
             # Wait until replication catches up.
-            while not self.log or self.log[-1].ts + D < query_start:
+            while not self.log or self.log[-1].optime.ts + D < query_start:
                 # TODO: abort on role change or rollback.
-                await Sleep.random_duration()
+                await sleep_random(MAX_DELAY)
 
         # Speculative majority wait.
-        last_written_ts = self.data[key]
-        while self.committed_ts < last_written_ts:
+        last_written_optime = self.data[key]
+        while self.committed_optime < last_written_optime:
             # TODO: abort on role change or rollback.
-            await Sleep.random_duration()
+            await sleep_random(MAX_DELAY)
 
-        return last_written_ts
+        return last_written_optime
 
 
-async def main(loop: "Loop"):
+async def client(client_id: int, primary: Node, secondary: Node):
+    write_optime = await primary.write(key="x")
+    logger.info(f"client {client_id} write OpTime: {write_optime}")
+    read_optime = await secondary.read(key="x")
+    logger.info(f"client {client_id} secondary read OpTime: {read_optime}")
+    assert read_optime >= write_optime, f"client {client_id} stale read"
+
+
+async def main():
     primary = Node(role=Role.PRIMARY)
     secondaries = [Node(role=Role.SECONDARY), Node(role=Role.SECONDARY)]
     nodes = [primary] + secondaries
     for n in nodes:
-        n.initiate(loop, nodes)
+        n.initiate(nodes)
 
-    write_result = await primary.write(key="x")
-    print(f"Write result: {write_result}")
-    assert write_result is Result.OK
+    lp = get_event_loop()
+    t1 = lp.create_task(client(client_id=1, primary=primary, secondary=secondaries[0]))
+    t2 = lp.create_task(client(client_id=2, primary=primary, secondary=secondaries[0]))
 
-    primary_read_result: Timestamp = await primary.read(key="x")
-    print(f"Primary read result: {primary_read_result}")
-
-    secondary_read_result = await secondaries[0].read(key="x")
-    print(f"secondary read result: {secondary_read_result}")
-    loop.stop()
+    await t1
+    await t2
+    lp.stop()
 
 
 if __name__ == "__main__":
-    event_loop = Loop()
-    event_loop.call_soon(main, event_loop)
+    initiate_logging()
+    event_loop = get_event_loop()
+    event_loop.create_task(main())
     event_loop.run()
     print(f"Finished after {get_current_ts()} ms (simulated)")
